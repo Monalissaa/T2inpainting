c351882574a9868b35d36f7768d53efff79310fc
mona <937315849@qq.com>
2022-11-06 18:29:34 +0800

    tsa all code

--- saicinpainting/training/modules/__init__.py
+++ saicinpainting/training/modules/__init__.py
@@ -4,7 +4,7 @@ from saicinpainting.training.modules.ffc import FFCResNetGenerator, FFCResNetGen
     FFCResNetGeneratorMaxChangeAdd, FFCResNetGeneratorSmall, FFCResNetGeneratorDropout, FFCResNetFSMRGenerator, FFCResNetFixAddFFCGenerator, \
         TsaFourFFCResNetGenerator, TsaTwoFFCResNetGenerator, TsaOneFFCResNetGenerator, TsaG2GFFCResNetGenerator, TsaG2GL2GFFCResNetGenerator, \
             TsaG2GL2LFFCResNetGenerator, TsaG2GConvL2LFFCResNetGenerator, TsaG2GDownUpFFCResNetGenerator, TsaG2GConvL2GFFCResNetGenerator, \
-                TsaMiddleAllConvFFCResNetGenerator
+                TsaMiddleAllConvFFCResNetGenerator, TsaAllConvFFCResNetGenerator
 from saicinpainting.training.modules.pix2pixhd import GlobalGenerator, MultiDilatedGlobalGenerator, \
     NLayerDiscriminator, MultidilatedNLayerDiscriminator, WaveNLayerDiscriminator
 
@@ -53,6 +53,8 @@ def make_generator(config, kind, **kwargs):
             return TsaG2GConvL2GFFCResNetGenerator(**kwargs)
         elif config.new_params.tsa.middle_all:
             return TsaMiddleAllConvFFCResNetGenerator(**kwargs)
+        elif config.new_params.tsa.all:
+            return TsaAllConvFFCResNetGenerator(**kwargs)
         elif config.new_params.tsa.g2g:
             return TsaG2GFFCResNetGenerator(**kwargs) 
         else:
--- saicinpainting/training/modules/ffc.py
+++ saicinpainting/training/modules/ffc.py
@@ -389,6 +389,89 @@ class FFCResNetGenerator(nn.Module):
         # return input
         return self.model(input)
 
+
+class TsaAllConvFFCResNetGenerator(nn.Module):
+    def __init__(self, input_nc, output_nc, ngf=64, n_downsampling=3, n_blocks=9, norm_layer=nn.BatchNorm2d,
+                 padding_type='reflect', activation_layer=nn.ReLU,
+                 up_norm_layer=nn.BatchNorm2d, up_activation=nn.ReLU(True),
+                 init_conv_kwargs={}, downsample_conv_kwargs={}, resnet_conv_kwargs={},
+                 spatial_transform_layers=None, spatial_transform_kwargs={},
+                 add_out_act=True, max_features=1024, out_ffc=False, out_ffc_kwargs={}):
+        assert (n_blocks >= 0)
+        super().__init__()
+
+        model = [nn.ReflectionPad2d(3),
+                 TsaDownFFC_BN_ACT(input_nc, ngf, kernel_size=7, padding=0, norm_layer=norm_layer,
+                            activation_layer=activation_layer, **init_conv_kwargs)]
+
+        ### downsample
+        for i in range(n_downsampling):
+            mult = 2 ** i
+            if i == n_downsampling - 1:
+                cur_conv_kwargs = dict(downsample_conv_kwargs)
+                cur_conv_kwargs['ratio_gout'] = resnet_conv_kwargs.get('ratio_gin', 0)
+            else:
+                cur_conv_kwargs = downsample_conv_kwargs
+            model += [TsaDownFFC_BN_ACT(min(max_features, ngf * mult),
+                                 min(max_features, ngf * mult * 2),
+                                 kernel_size=3, stride=2, padding=1,
+                                 norm_layer=norm_layer,
+                                 activation_layer=activation_layer,
+                                 **cur_conv_kwargs)]
+
+        mult = 2 ** n_downsampling
+        feats_num_bottleneck = min(max_features, ngf * mult)
+
+        ### resnet blocks
+        for i in range(n_blocks):
+            cur_resblock = TsaMiddleAllConvFFCResnetBlock(feats_num_bottleneck, padding_type=padding_type,
+                                                       activation_layer=activation_layer,
+                                                       norm_layer=norm_layer, **resnet_conv_kwargs)
+            if spatial_transform_layers is not None and i in spatial_transform_layers:
+                cur_resblock = LearnableSpatialTransformWrapper(cur_resblock, **spatial_transform_kwargs)
+            model += [cur_resblock]
+
+        model += [ConcatTupleLayer()]
+        model_up_alpha = []
+        ### upsample
+        for i in range(n_downsampling):
+            mult = 2 ** (n_downsampling - i)
+            model += [nn.ConvTranspose2d(min(max_features, ngf * mult),
+                                         min(max_features, int(ngf * mult / 2)),
+                                         kernel_size=3, stride=2, padding=1, output_padding=1),
+                      up_norm_layer(min(max_features, int(ngf * mult / 2))),
+                      up_activation]
+            model_up_alpha += [nn.ConvTranspose2d(min(max_features, ngf * mult),
+                                         min(max_features, int(ngf * mult / 2)),
+                                         kernel_size=3, stride=2, padding=1, output_padding=1)]
+
+        if out_ffc:
+            model += [TsaMiddleAllConvFFCResnetBlock(ngf, padding_type=padding_type, activation_layer=activation_layer,
+                                                  norm_layer=norm_layer, inline=True, **out_ffc_kwargs)]
+
+        model += [nn.ReflectionPad2d(3),
+                  nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model_up_alpha += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        if add_out_act:
+            model.append(get_activation('tanh' if add_out_act is True else add_out_act))
+        self.model = nn.Sequential(*model)
+        self.model_up_alpha = nn.Sequential(*model_up_alpha)
+        # self.pa = pa(feat_dim=4)
+
+    def forward(self, input, pa=False):
+        for block in self.model[:-12]:
+            input = block(input)
+        up_loc = 0
+        for block in self.model[-12:]:
+            if block.__class__.__name__ == 'ConvTranspose2d' or block.__class__.__name__ == 'Conv2d':
+                input = block(input) + self.model_up_alpha[up_loc](input)
+                up_loc += 1
+            else:
+                input = block(input)
+        return input
+
+
+
 class TsaDownFFC(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size,
                  ratio_gin, ratio_gout, stride=1, padding=0,
